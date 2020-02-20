2c439bec6e8e30a743259a537b5ec56884c1afe2
wirlleyd <wirlley_d@hotmail.com>
2020-02-19 21:59:03 -0300

    Updates

---
+++ precache-manifest.67e3d3db96c7a7925fccee676ac869aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1acdf5ba130d49801b172f272812b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "82fb50cdbc0fdccfa6dd",
+    "url": "/monsters-rolodex/static/css/main.0e91b7f9.chunk.css"
+  },
+  {
+    "revision": "2ce761e7d88f63aed3c5",
+    "url": "/monsters-rolodex/static/js/2.dbc0e4af.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.dbc0e4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fb50cdbc0fdccfa6dd",
+    "url": "/monsters-rolodex/static/js/main.c4bbfa3e.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
